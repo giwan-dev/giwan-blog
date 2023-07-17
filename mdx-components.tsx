5f1109275104d7a6e46e45dc91f3ec201e0e7eb0
giwan <giwan.dev@gmail.com>
2023-07-17 23:10:28 +0900

    💄 MDX가 렌더링하는 태그 일부의 스타일 적용
    h1, h2, p, code, a 태그의 스타일을 작성했습니다.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -11,5 +11,21 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
     ...components,
+    h1: ({ children }) => (
+      <h1 className="mb-8 text-4xl font-bold">{children}</h1>
+    ),
+    h2: ({ children }) => <h2 className="text-2xl mt-6 mb-3">{children}</h2>,
+    p: ({ children }) => <p className="my-2">{children}</p>,
+    code: ({ children }) => (
+      <code className="rounded-md px-1 py-0.5 bg-gray-200">{children}</code>
+    ),
+    a: ({ href, children }) => (
+      <a
+        href={href}
+        className="transition-colors text-blue-500 hover:text-blue-400"
+      >
+        {children}
+      </a>
+    ),
   }
 }
