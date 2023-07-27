c05f25794f18340ebba799957114e89592df1124
giwan <giwan.dev@gmail.com>
2023-07-28 01:04:33 +0900

    ♿️ 색 대비가 충분하도록 조정

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -5,12 +5,12 @@ export function PostTitle({ title }: { title: string }) {
     <div className="mb-8 text-4xl font-bold">
       <Link
         href="/"
-        className="!border-none !text-gray-300 hover:!text-gray-200"
+        className="!border-none !text-gray-500 hover:!text-gray-400"
       >
         <span>글</span>
       </Link>
 
-      <span className="text-gray-200"> / </span>
+      <span className="text-gray-500"> / </span>
 
       <h1 className="inline">{title}</h1>
     </div>
