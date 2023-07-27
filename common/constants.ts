bbda3dc9e45ee4e37f6de25efbfc2be37d749fb7
giwan <giwan.dev@gmail.com>
2023-07-27 21:13:22 +0900

    🐛 base URL 하드코딩
    VERCEL_URL에는 deployment specific한 URL이 들어있어서 sitemap으로는 적합하지 않았습니다.

--- common/constants.ts
+++ common/constants.ts
@@ -1,3 +1,5 @@
 export const METADATA_TITLE_POSTFIX = "- 박기완의 블로그"
 
-export const BASE_URL = `http://${process.env.VERCEL_URL ?? "localhost:3000"}`
+export const BASE_URL = process.env.VERCEL
+  ? "https://giwan-blog.vercel.app"
+  : "http://localhost:3000"
