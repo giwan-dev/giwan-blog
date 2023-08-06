4fc5ad890757ad5e147e673fad2d89f0f1d9a1d6
giwan <giwan.dev@gmail.com>
2023-08-06 12:01:47 +0900

    🎨 블로그 포스트로 작성한 mdx의 타입 정의 위치 이동
    별도 파일 없이 index 파일에서 타입을 정의합니다.

--- posts/index.ts
+++ posts/index.ts
@@ -1,6 +1,22 @@
+import { MDXProps } from "mdx/types"
+
 export async function importPost(slug: string) {
   const { default: MDXContent, meta }: Awaited<typeof import("@/posts/*.mdx")> =
     await import(`@/posts/${slug}.mdx`)
 
   return { MDXContent, meta }
+}
+
+declare module "@/posts/*.mdx" {
+  type ISO8601 = string
+
+  interface PostMeta {
+    title: string
+    description: string
+    createdAt: ISO8601
+    updatedAt: ISO8601 | undefined
+  }
+
+  export const meta: PostMeta
+  export default function MDXContent(props: MDXProps): JSX.Element
 }
