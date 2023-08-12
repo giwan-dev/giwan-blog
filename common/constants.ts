28884f488abc1d308abe01f86e5265a3034fe91d
giwan <giwan.dev@gmail.com>
2023-08-12 22:28:53 +0900

    ✨ 스포티파이 관련 상수 정의
    필수 값이기 때문에 이를 검사하는 함수를 작성합니다.

--- common/constants.ts
+++ common/constants.ts
@@ -3,3 +3,15 @@ export const METADATA_TITLE_POSTFIX = "- 박기완의 블로그"
 export const BASE_URL = process.env.VERCEL
   ? "https://giwan-blog.vercel.app"
   : "http://localhost:3000"
+export const SPOTIFY_CLIENT_ID = required(process.env.SPOTIFY_CLIENT_ID)
+export const SPOTIFY_CLIENT_SECRET = required(process.env.SPOTIFY_CLIENT_SECRET)
+
+function required(
+  value: string | undefined,
+): string {
+  if (value === undefined) {
+    throw new Error("This value should be non nullable")
+  }
+
+  return value
+}
