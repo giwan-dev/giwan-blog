f09f0a0a3d0fa6e5c9279fa039150d2e6e63b30d
giwan <giwan.dev@gmail.com>
2023-08-28 14:18:55 +0900

    🛂 클라이언트 환경에서 Spotify의 access token에 접근할 수 있도록 세션에 값 전달
    next-auth가 제공하는 콜백 시스템을 사용했습니다.
    jwt 콜백은 JSON web token을 만들거나 업데이트할 때 호출합니다.
    이때 로그인한 계정 정보에 접근할 수 있는데 여기서 access token을 가져와서 토큰 객체에 넣어줍니다.
    session 콜백은 클라이언트에서 세션에 접근할 때 호출합니다.
    이 때 토큰 객체에 들어가 있던 access token을 세션 객체로 넘겨줍니다.
    
    Session, JWT 모두 추가로 넘기는 값에 대한 타이핑이 되어있지 않기 때문에 모듈 확장으로 이를 타이핑해줍니다.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,20 @@
 import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from "@/common/constants"
-import { AuthOptions } from "next-auth"
+import type { AuthOptions, DefaultSession } from "next-auth"
 import NextAuth from "next-auth/next"
 import SpotifyProvider from "next-auth/providers/spotify"
+import type { DefaultJWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken: string
+  }
+}
 
 const authOptions: AuthOptions = {
   providers: [
@@ -10,6 +23,26 @@ const authOptions: AuthOptions = {
       clientSecret: SPOTIFY_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    jwt({ account, token }) {
+      // NOTE: 타입 정의는 Account | null인데 실제 값은 undefined
+      if (!account) {
+        return token
+      }
+
+      const accessToken = account.access_token
+      if (accessToken === undefined) {
+        throw new Error("Access Token이 없습니다.")
+      }
+
+      token.accessToken = accessToken
+      return token
+    },
+    session({ token, session }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
 }
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions)
